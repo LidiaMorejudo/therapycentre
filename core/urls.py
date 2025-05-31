e758c1382b916cde8903f0b2c4c81feef89be06d
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-05-31 15:14:28 +0100

    Updated urls.py with the urls of the two new pages findus and sessions

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('findus/', views.findus, name='findus'),
+    path('sessions/', views.sessions, name='sessions'),
 ]
