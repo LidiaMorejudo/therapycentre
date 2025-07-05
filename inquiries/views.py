e80df0053311cf1b3c720ad0ea48b8791068a835
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-07-05 16:51:15 +0100

    Added code to views to render the contact form on enquiries

--- inquiries/views.py
+++ inquiries/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import EnquiryForm  # import the form
 
 def inquiries_view(request):
-    return render(request, 'inquiries/enquiries.html')
+    if request.method == 'POST':
+        form = EnquiryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/enquiries/?success=true')  # optional confirmation modal
+    else:
+        form = EnquiryForm()
+
+    return render(request, 'inquiries/enquiries.html', {'enquiry_form': form})
 
