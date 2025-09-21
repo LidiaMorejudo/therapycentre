e8ab6e3ed72b209ccfee4617d0e2f65e15edf001
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-09-21 12:52:21 +0100

    added first lines of codes to views.py to prepare the view of the form in the booking app

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from .forms import BookATableForm
+from .models import BookATable
 
 def booking(request):
     return render(request, 'booking/booking.html')
