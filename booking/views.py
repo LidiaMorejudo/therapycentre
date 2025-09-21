4aaa0a0dd3b1ef64d870b200099715d8d68f6564
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-09-21 12:53:40 +0100

    Changed code to match fields in forms to session

--- booking/views.py
+++ booking/views.py
@@ -3,8 +3,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-from .forms import BookATableForm
-from .models import BookATable
+from .forms import BookASessionForm
+from .models import BookASession
 
 def booking(request):
     return render(request, 'booking/booking.html')
