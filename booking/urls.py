86383aeece6c1ee8878877072cd71377c73a03d3
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-11-23 12:41:55 +0000

    modified urls.py with worrect links

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,11 @@
-# booking/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.book_session, name='booking'),
+    path('bookings/', views.booking_list, name='booking_list'),
+    path('booking/edit/<int:booking_id>/', views.booking_edit, name='booking_edit'),
+    path('booking/edit/confirmation/', views.booking_edit_confirmation, name='booking_edit_confirmation'),
+    path('booking/delete/<int:booking_id>/', views.booking_delete, name='booking_delete'),
+    path('booking/delete/confirmation/', views.booking_delete_confirmation, name='booking_delete_confirmation'),
 ]
