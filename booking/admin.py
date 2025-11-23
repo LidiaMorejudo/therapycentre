78d4d292de373cb3360b920f7d41dfc1e5a6e68c
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-11-23 13:42:13 +0000

    added code to admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BookASession
 
-# Register your models here.
+@admin.register(BookASession)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'firstname', 'lastname', 'email',
+        'date', 'time', 'students', 'read', 'user'
+    )
+    list_filter = ('date', 'time', 'read')
+    search_fields = ('firstname', 'lastname', 'email')
