fe337d96fb3387584f3123599d2bd4fea59066f1
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-11-26 22:45:06 +0000

    added code to urls.py

--- therapycentre/urls.py
+++ therapycentre/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from booking import views as booking_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),             # homepage, sessions, etc.
     path('inquiries/', include('inquiries.urls')), # inquiries is mounted at /inquiries/
     path('booking/', include('booking.urls')),  # mounted at /booking/
+
+     # Authentication
+    path('accounts/', include('django.contrib.auth.urls')),  # login/logout/password reset
+    path('signup/', booking_views.signup, name='signup'),    # user registration
+    path('', include('booking.urls')),
 ]
 
