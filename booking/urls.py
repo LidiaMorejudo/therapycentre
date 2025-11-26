baa8971088811abeb1047997fb20c64970ee36f2
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-11-26 22:42:15 +0000

    updating it to match new code added for signing up

--- booking/urls.py
+++ booking/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.book_session, name='booking'),
-    path('bookings/', views.booking_list, name='booking_list'),
-    path('booking/edit/<int:booking_id>/', views.booking_edit, name='booking_edit'),
-    path('booking/edit/confirmation/', views.booking_edit_confirmation, name='booking_edit_confirmation'),
-    path('booking/delete/<int:booking_id>/', views.booking_delete, name='booking_delete'),
-    path('booking/delete/confirmation/', views.booking_delete_confirmation, name='booking_delete_confirmation'),
+    path('list/', views.booking_list, name='booking_list'),
+
+    path('edit/<int:booking_id>/', views.booking_edit, name='booking_edit'),
+    path('edit/confirmation/', views.booking_edit_confirmation, name='booking_edit_confirmation'),
+
+    path('delete/<int:booking_id>/', views.booking_delete, name='booking_delete'),
+    path('delete/confirmation/', views.booking_delete_confirmation, name='booking_delete_confirmation'),
+
+    # User system
+    path('signup/', views.signup, name='signup'),
 ]
+
