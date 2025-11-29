b1c4a826cf93531f860f3ab7e2f87fa410b9e33c
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-11-29 13:42:39 +0000

    added fields to admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from .models import BookASession
 
 @admin.register(BookASession)
-class BookingAdmin(admin.ModelAdmin):
+class BookASessionAdmin(admin.ModelAdmin):
     list_display = (
         'firstname', 'lastname', 'email',
         'date', 'time', 'students', 'read', 'user'
     )
     list_filter = ('date', 'time', 'read')
     search_fields = ('firstname', 'lastname', 'email')
+    # Ordering by newest date and time first
+    ordering = ('-date', '-time')
+
+    # Fields to display in the add/edit form
+    fields = (
+        'firstname', 'lastname', 'email',
+        'date', 'time', 'students', 'message', 'read', 'user'
+    )
