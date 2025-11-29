f0bc8ed8b3d18b6a7166f28c48978fc6367be787
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-11-29 14:03:58 +0000

    added code to signals.py

---
+++ booking/signals.py
@@ -0,0 +1,22 @@
+import logging
+from allauth.account.signals import user_signed_up
+from django.dispatch import receiver
+from .models import BookASession
+
+logger = logging.getLogger(__name__)
+
+@receiver(user_signed_up)
+def handle_user_signed_up(sender, request, user, **kwargs):
+    email = user.email
+    logger.info(f"User signed up with email: {email}")
+
+    # 
+    bookings = BookASession.objects.filter(email=email, user__isnull=True)
+    logger.info(f"Found {bookings.count()} bookings for this email")
+
+    if bookings.exists():
+        # 
+        bookings.update(user=user)
+        logger.info(f"Updated bookings for user: {user}")
+    else:
+        logger.info(f"No bookings found for email: {email}")
