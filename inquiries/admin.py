c77c821367583e2b1da6601b9955d3432674b9bf
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-12-09 22:20:06 +0000

    Registered app in admin.py

--- inquiries/admin.py
+++ inquiries/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Inquiries
 
-# Register your models here.
+@admin.register(Inquiries)
+class InquiriesAdmin(admin.ModelAdmin):
+    list_display = ("subject", "name", "email", "read")
+    list_filter = ("read",)
+    search_fields = ("name", "email", "subject")
