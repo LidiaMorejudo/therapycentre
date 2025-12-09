4d838eeb66a9562c7561db8ef1899514b85c78ee
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-12-09 22:35:42 +0000

    Changed inquiries to inquiry

--- inquiries/admin.py
+++ inquiries/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Inquiries
+from .models import Inquiry
 
-@admin.register(Inquiries)
-class InquiriesAdmin(admin.ModelAdmin):
+@admin.register(Inquiry)
+class InquiryAdmin(admin.ModelAdmin):
     list_display = ("subject", "name", "email", "read")
     list_filter = ("read",)
     search_fields = ("name", "email", "subject")
