80c864af8e192c545d0dd49ede59467963e732d1
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-12-28 17:15:29 +0000

    Added lines and shorter lines in views.py in core

--- core/views.py
+++ core/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'core/index.html')
+
 
 def findus(request):
     return render(request, 'core/findus.html')
 
+
 def sessions(request):
     return render(request, 'core/sessions.html')
+
 
 def booking_view(request):
     return render(request, 'core/booking.html')
