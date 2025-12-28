3b21156bcb0a17b725474297bad3840e4ee79a1e
LidiaMorejudo <163455644+LidiaMorejudo@users.noreply.github.com>
2025-12-28 17:05:55 +0000

    Added spaces and shorter lines in urls.py in booking

--- booking/urls.py
+++ booking/urls.py
@@ -2,12 +2,37 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.book_session, name='booking'),  # main booking page
-    path('my-bookings/', views.booking_list, name='booking_list'),  # list of user's bookings
+    path(
+        '',
+        views.book_session,
+        name='booking'
+    ),  # main booking page
 
-    path('edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
-    path('edit/confirmation/', views.edit_booking_confirmation, name='edit_booking_confirmation'),
+    path(
+        'my-bookings/',
+        views.booking_list,
+        name='booking_list'
+    ),  # list of user's bookings
 
-    path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
-    path('delete/confirmation/', views.delete_booking_confirmation, name='delete_booking_confirmation'),
+    path(
+        'edit/<int:booking_id>/',
+        views.edit_booking,
+        name='edit_booking'
+    ),
+    path(
+        'edit/confirmation/',
+        views.edit_booking_confirmation,
+        name='edit_booking_confirmation'
+    ),
+
+    path(
+        'delete/<int:booking_id>/',
+        views.delete_booking,
+        name='delete_booking'
+    ),
+    path(
+        'delete/confirmation/',
+        views.delete_booking_confirmation,
+        name='delete_booking_confirmation'
+    ),
 ]
